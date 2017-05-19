60fe2b610771a248c286893b451ae81de53a25d6
Brian Sokas <briansokas@Brians-MBP.PK5001Z>
2017-05-18 20:08:18 -0600

    Added fields to the database, added a url and basic index view method

--- uDodo/cheat_entry/models.py
+++ uDodo/cheat_entry/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
 
-class CheatForm(models.Model):
+class CheatApplication(models.Model):
     name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    class_size = models.IntegerField(default=0)
+    num_q = models.IntegerField(default=0)
+    date_occurred = models.DateTimeField
+
--- uDodo/uDodo/settings.py
+++ uDodo/uDodo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cheat_entry.apps.CheatEntryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
