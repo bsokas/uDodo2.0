b1a52def1f7f7da89aa7bbc5945e1be2636f4210
bsokas <bsokas@seas.upenn.edu>
2017-05-27 15:16:55 -0600

    Added home to the Installed Apps settings block

--- uDodo/uDodo/settings.py
+++ uDodo/uDodo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cheat_entry.apps.CheatEntryConfig',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
