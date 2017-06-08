8677d1c9219a09d99fce9349763525e67e8b1a66
bsokas <bsokas@seas.upenn.edu>
2017-06-07 18:55:56 -0600

    Making up for all the changes that I made in the last two weeks.

--- uDodo/cheat_entry/urls.py
+++ uDodo/cheat_entry/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = "cheat_entry"
-
+app_name = 'cheat_entry'
 urlpatterns = [
 
-    url(r'^$', views.home, name="Home"),
-    url(r'^successful_post$', views.submission, name="successful_post")
+    #Initial url for entering a cheating entry
+    url(r'^$', views.home, name="index"),
+
+    #Simple HTML page that displays successful submission
+    url(r'^successful_post$', views.submission, name="successful_post"),
 
 ]
 
