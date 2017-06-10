5ceef4efb4361492b0aad49afb6f9694ab817dcc
bsokas <bsokas@seas.upenn.edu>
2017-06-10 12:23:35 -0600

    Fixed successful_post url addressing

--- uDodo/cheat_entry/urls.py
+++ uDodo/cheat_entry/urls.py
@@ -3,15 +3,16 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'cheat_entry'
-urlpatterns = [
+urlpatterns = {
 
-    #Initial url for entering a cheating entry
+    # Initial url for entering a cheating entry
     url(r'^$', views.home, name="index"),
 
-    #Simple HTML page that displays successful submission
-    url(r'^successful_post$', views.submission, name="successful_post"),
+    # Simple HTML page that displays successful submission
+    # supposed to be cheat_entry/successful_post
+    url(r'^/successful_post/$', views.submission, name="successful_post"),
 
-]
+}
 
 
 #Used for defining urls
